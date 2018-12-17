371c67445ec551220ee60078f04c5ff733187bb2
kcvet <kc3536@student.uni-lj.si>
2018-12-17 20:02:13 +0100

    added profile pics, user profile now shows your own profile

--- app/src/main/java/com/tpo/groupy/Tab2Fragment.java
+++ app/src/main/java/com/tpo/groupy/Tab2Fragment.java
@@ -2,12 +2,16 @@ package com.tpo.groupy;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +26,10 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +40,7 @@ public class Tab2Fragment extends Fragment {
     private RequestQueue requestQueue;
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
+    ImageView profile_pic;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,6 +51,7 @@ public class Tab2Fragment extends Fragment {
         phone = (TextView)view.findViewById(R.id.phone);
         email = (TextView)view.findViewById(R.id.email) ;
         sex = (TextView)view.findViewById(R.id.sex);
+        profile_pic = (ImageView)view.findViewById(R.id.profile_pic);
         requestQueue = Volley.newRequestQueue(getActivity());
 
         prefs = this.getActivity().getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
@@ -56,8 +66,8 @@ public class Tab2Fragment extends Fragment {
     private void getUserInfo() {
 
 
-        int user_id = Tab2Fragment.getInt
-        String url = "http://grupyservice.azurewebsites.net/UserService.svc/41";
+        int user_id = prefs.getInt("ID_USER",0);
+        String url = "http://grupyservice.azurewebsites.net/UserService.svc/"+user_id;
 
 
         JsonObjectRequest arrReq = new JsonObjectRequest(Request.Method.GET, url,null,
@@ -72,10 +82,21 @@ public class Tab2Fragment extends Fragment {
                             String name_r = response.getString("name");
                             String surname_r = response.getString("surname");
                             String sex_r = response.getString("sex");
+                            String phone_r = response.getString("phone");
+                            String profile_pic_r = response.getString("profile_pic");
+
+
+                            new DownloadImageTask(profile_pic)
+                                    .execute(profile_pic_r);
+                            /*
+                            profile_pic.setImageBitmap(bmp);
+
+*/
 
                             sex.setText(sex_r);
                             email.setText(email_r);
                             name.setText(name_r+ " "+surname_r);
+                            phone.setText(phone_r);
 
                         }catch (JSONException e){
                             e.printStackTrace();
@@ -127,4 +148,30 @@ public class Tab2Fragment extends Fragment {
 
 
     }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
+
