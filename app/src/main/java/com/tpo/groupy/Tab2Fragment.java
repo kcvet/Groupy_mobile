d98c5b7dcaa3b9347aed78c292cc60cce8d9218a
kcvet <kc3536@student.uni-lj.si>
2018-12-23 17:37:40 +0100

    added some icons, made user info updatable

--- app/src/main/java/com/tpo/groupy/Tab2Fragment.java
+++ app/src/main/java/com/tpo/groupy/Tab2Fragment.java
@@ -11,7 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +31,7 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,37 +43,89 @@ import java.util.Map;
 
 public class Tab2Fragment extends Fragment {
 
-    public TextView name, phone, email, sex;
+    public TextView name;
+    public int editable = 0; //to check if we make data editaable, or send it to update data
+    public Button user_settings;
+    EditText email, phone, introduction;
     private RequestQueue requestQueue;
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
     ImageView profile_pic;
-
+    Spinner sex;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_two, container, false);
 
 
+
         name = (TextView)view.findViewById(R.id.user_name);
-        phone = (TextView)view.findViewById(R.id.phone);
-        email = (TextView)view.findViewById(R.id.email) ;
-        sex = (TextView)view.findViewById(R.id.sex);
+        phone = (EditText) view.findViewById(R.id.phone);
+        email = (EditText) view.findViewById(R.id.email) ;
+        sex = (Spinner) view.findViewById(R.id.sex);
+        introduction= (EditText) view.findViewById(R.id.introduction);
         profile_pic = (ImageView)view.findViewById(R.id.profile_pic);
+        user_settings = (Button)view.findViewById(R.id.user_settings);
         requestQueue = Volley.newRequestQueue(getActivity());
+        String[] arraySpinner = new String[] {
+                "Male", "Female", "Non-binary"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sex.setAdapter(adapter);
 
         prefs = this.getActivity().getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
         editor = prefs.edit();
 
         getUserInfo();
 
+        user_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(editable == 0) setEditable();
+                else updateUserInfo();
+            }
+        });
+
         return view;
 
+
+
+    }
+
+    private void updateUserInfo() {
+        introduction.setFocusable(false);
+        introduction.setClickable(false);
+        phone.setFocusable(false);
+        phone.setClickable(false);
+        email.setClickable(false);
+        email.setFocusable(false);
+        sex.setClickable(false);
+        sex.setFocusable(false);
+        user_settings.setText("Edit info");
+
+        editable = 0;
+    }
+    private void setEditable(){
+        introduction.setFocusableInTouchMode(true);
+        introduction.setClickable(true);
+        phone.setFocusableInTouchMode(true);
+        phone.setClickable(true);
+        email.setClickable(true);
+        email.setFocusableInTouchMode(true);
+        sex.setClickable(true);
+        sex.setFocusableInTouchMode(true);
+        user_settings.setText("Update");
+        Toast.makeText(getActivity(), "this is some straight A bullshit", Toast.LENGTH_LONG);
+        editable = 1;
     }
 
     private void getUserInfo() {
 
 
         int user_id = prefs.getInt("ID_USER",0);
+        final String em = prefs.getString("user", "none");
         String url = "http://grupyservice.azurewebsites.net/UserService.svc/"+user_id;
 
 
@@ -81,9 +140,11 @@ public class Tab2Fragment extends Fragment {
                             String email_r= response.getString("email");
                             String name_r = response.getString("name");
                             String surname_r = response.getString("surname");
+                            String introduction_r = response.getString("introduction");
                             String sex_r = response.getString("sex");
                             String phone_r = response.getString("phone");
                             String profile_pic_r = response.getString("profile_pic");
+
 
 
                             new DownloadImageTask(profile_pic)
@@ -92,8 +153,8 @@ public class Tab2Fragment extends Fragment {
                             profile_pic.setImageBitmap(bmp);
 
 */
-
-                            sex.setText(sex_r);
+                            introduction.setText(introduction_r);
+                            //sex.setText(sex_r);
                             email.setText(email_r);
                             name.setText(name_r+ " "+surname_r);
                             phone.setText(phone_r);
