ba457d15cb67453b08a7791cbb4c58b2e7002c4c
Peho <anze.peharc@gmail.com>
2018-12-26 18:53:24 +0100

    Added simple GET to CardAdapter onClick

--- app/src/main/java/com/tpo/groupy/CardAdapter.java
+++ app/src/main/java/com/tpo/groupy/CardAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,9 +16,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Ravi Tamada on 18/05/16.
@@ -28,6 +41,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> 
     private List<Card> albumList;
     LinearLayout layout;
     private View subItem;
+    private RequestQueue requestQueue;
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView title, count;
@@ -35,6 +49,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> 
 
         public MyViewHolder(View view) {
             super(view);
+            requestQueue = Volley.newRequestQueue(view.getContext());
             title = (TextView) view.findViewById(R.id.title);
             count = (TextView) view.findViewById(R.id.count);
             thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
@@ -132,7 +147,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> 
             switch (menuItem.getItemId()) {
                 case R.id.action_add_favourite:
                     Toast.makeText(mContext, "You successfully joined the group!", Toast.LENGTH_SHORT).show();
-
+                    joinGroup();
                     return true;
                 case R.id.action_play_next:
                     Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
@@ -185,6 +200,70 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> 
             });
             animator.start();
         }
+
+    }
+
+    private void joinGroup(){
+
+        String url = "http://grupyservice.azurewebsites.net/GroupService.svc/";
+
+
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        // The user does have repos, so let's loop through them all
+                        //JSONObject obj = response;
+                        try{
+                            JSONObject jsonobject = response.getJSONObject(0);
+                            Toast.makeText(mContext, jsonobject.toString(), Toast.LENGTH_SHORT).show();
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        Toast.makeText(mContext, error.toString(), Toast.LENGTH_SHORT).show();
+
+                        //setRepoListText(String.valueOf(a));
+                        Log.e("Volley", error.toString());
+                    }
+
+
+                }
+
+        ){
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> paramsValue = new HashMap<String, String>();
+                // paramsValue.put("abc@abc.com", new String("geslo123"));
+                return paramsValue;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                return headers;
+            }
+        };
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+
+        requestQueue.add(arrReq);
+
 
     }
 
