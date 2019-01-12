c3920921d9a8ba59cb72e08b6d5f5ea0a8adc873
kcvet <kc3536@student.uni-lj.si>
2019-01-12 19:57:12 +0100

    made it a little prettier, not by much thou

--- app/src/main/java/com/tpo/groupy/SentMessageHolder.java
+++ app/src/main/java/com/tpo/groupy/SentMessageHolder.java
@@ -5,12 +5,14 @@ import android.view.View;
 import android.widget.TextView;
 
 public class SentMessageHolder extends RecyclerView.ViewHolder {
-    TextView messageText, timeText;
+    TextView messageText, timeText, nameText;
 
     SentMessageHolder(View itemView) {
         super(itemView);
         messageText = (TextView) itemView.findViewById(R.id.text_message_body);
         timeText = (TextView) itemView.findViewById(R.id.text_message_time);
+        nameText = (TextView) itemView.findViewById(R.id.text_message_name);
+
     }
 
     void bind(String message, String date) {
